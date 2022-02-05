af86f8abb31925415b698b93d05d7a26cc4de3e7
saiyadm99 <syedmohammod52@gmail.com>
2022-02-06 03:11:10 +0600

    test 1.2

--- server.js
+++ server.js
@@ -17,10 +17,27 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: {
-			rejectUnauthorized: false
-		}
+    ssl: true,
   }
+});
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
 
