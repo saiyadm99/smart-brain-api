f3b0fafaa7a6bfe659fd28999b392c5a219fb865
saiyadm99 <syedmohammod52@gmail.com>
2022-02-08 03:34:08 +0600

    2.2

--- server.js
+++ server.js
@@ -13,23 +13,14 @@ const image = require('./controllers/image');
 //const { response } = require('express');
 //const { user } = require('pg/lib/defaults');
 
-const { Client } = require('pg');
-
-const db = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: {
+			rejectUnauthorized: false
+		}
   }
-});
-
-db.connect();
-
-db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  db.end();
 });
 
 
